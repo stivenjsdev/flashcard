18550e2bc9af511fac4798151599456479fd4d58
stiven trujillo <strujillodev@gmail.com>
2024-09-24 19:02:12 -0500

    feat: center title

--- src/pages/DeckImporter.tsx
+++ src/pages/DeckImporter.tsx
@@ -70,7 +70,7 @@ const DeckImporter = () => {
 
   return (
     <div className="w-full max-w-md mx-auto py-4">
-      <h2 className="text-2xl font-bold mb-4 text-tertiary-normal">
+      <h2 className="text-2xl font-bold mb-4 text-tertiary-normal text-center">
         Importador de Decks
       </h2>
       <div className="mb-4">
